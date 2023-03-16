e39a994f2740bcbc90aed0465b6f463ac9971137
Rutik7294 <98601651+Rutik7294@users.noreply.github.com>
2023-03-16 12:28:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+
+        }else{
+            $('.navbar').removeClass("sticky");
+
+        }
+    });
+    
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+    //typing animation
+
+    var typed = new Typed(".typing",{
+        strings:["Python Developer","Web Designer","Data Science Enthusiast"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop :true
+    })
+
+    var typed = new Typed(".typing-2",{
+        strings:["Python Developer","Web Designer","Data Science Enthusiast"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop :true
+    })
+
+});
